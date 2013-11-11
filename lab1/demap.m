148b6d520c1ded0985484c16fb32930eb316d34f
Simon Maurer <moirelein@gmail.com>
2013-11-11 19:05:49 +0100

    so simple you would not believe it

--- lab1/demap.m
+++ lab1/demap.m
@@ -1,9 +1,5 @@
 function [bit2] = demap(symb)
 
-    b1 = ones(1, length(symb));
-    b0 = zeros(1, length(symb));
-    b1(imag(symb)(:) > 0) = 0;
-    b0(real(symb)(:) > 0) = 1;
-    bit2 = [b0; b1](:);
+    bit2 = [(real(symb) > 0)';(imag(symb) < 0)'](:);
 
 end
