b8dd463bde3c971b245d0b78dba32c0c876f2eb0
Simon Maurer <moirelein@gmail.com>
2013-11-12 20:05:02 +0100

    remove images

--- ber.m
+++
@@ -1,8 +0,0 @@
-function [bit_cnt bit_err_cnt rate] = ber(x_init, x_final)
-
-    bit_cnt = length(x_init(:));
-    diff_arr = x_init~=x_final;
-    bit_err_cnt = sum(diff_arr(:));
-    rate = bit_err_cnt/bit_cnt;
-
-end
