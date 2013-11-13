516bcdc0025a37d107264a04a2445f73ef3616b1
Simon Maurer <moirelein@gmail.com>
2013-11-13 21:47:31 +0100

    modify frame_sync (simplify)

--- frame_sync.m
+++ frame_sync.m
@@ -1,10 +1,17 @@
-function [res] = frame_sync(sig, p, lambda)
+function [res res2] = frame_sync(sig, lambda)
     % detect the starting point in a stream by matching a pattern
     % sig: input signal where the starting point needs to be found
     % p: pattern
     % lambda: detection treshold
     % return: staring index
-    Np = length(p);
+    pf = [1 0 0 0 1 1 1 0 1];
+    pinit = [1 1 1 1 1 1 1 1];
+    Np = 200;
+
+    p = lfsr(pf, pinit, Np, 1);
+
+    p(p == 1) = -1;
+    p(p == 0) = 1;
     for n=1:(length(sig)-Np+1),
         sig_w = sig(n:Np+n-1);
         cn_2 = abs(p*sig_w)^2;
--- lab2.m
+++ lab2.m
@@ -1,16 +1,8 @@
 tic
-pf = [1 0 0 0 1 1 1 0 1];
-pinit = [1 1 1 1 1 1 1 1];
-Np = 200;
 lambda = 30;
 sig = load('r_image_2_10.mat');
 
-p = lfsr(pf, pinit, Np, 1);
-
-p(p == 1) = -1;
-p(p == 0) = 1;
-
-start_point = frame_sync(sig.r, p, Np, lambda);
+start_point = frame_sync(sig.r, lambda);
 
 img_size = sig.image_size;
 img = sig.r(start_point:start_point+img_size(1)*img_size(2)*4-1);
