9ee8e8d70d2cd77a0198aeb983924282d947eb51
Simon Maurer <moirelein@gmail.com>
2013-11-16 12:19:18 +0100

    completed lab4

--- interpolate.m
+++ interpolate.m
@@ -1,8 +1,21 @@
-function [res] = interpolate(sig, epsilon, t='linear')
-    if t == 'linear',
+function [res] = interpolate(sig, epsilon, cubic=0)
+    % calcualte the interpolated signal
+    % sig: signal to interpolate
+    % epsilon: interpolation variable
+    % cubic: if 0, use linear interpolation, else cubic interpolation
+    % res: interpolated signal (synchronized)
+    if cubic == 1,
+        % cubic interpolation
+        epsilon = epsilon(1:length(sig));
+        sig = [0; sig; 0; 0];
+        a = -sig(1:end-3) + 3*sig(2:end-2) - 3*sig(3:end-1) + sig(4:end);
+        b = 3*sig(1:end-3) - 6*sig(2:end-2) + 3*sig(3:end-1);
+        c = (-2)*sig(1:end-3) - 3*sig(2:end-2) + 6*sig(3:end-1) - sig(4:end);
+        d = 6*sig(2:end-2);
+        res = a.*epsilon.^3 + b.*epsilon.^2 + c.*epsilon + d;
+    else
+        % linear interpolation
         epsilon = epsilon(1:length(sig) - 1);
         res = (1.-epsilon).*sig(1:end-1) + epsilon.*sig(2:end);
-    else
-        res = sig;
     end
 end
--- lab43.m
+++ lab43.m
@@ -2,13 +2,13 @@ taps = 20;
 T = 1;
 roll_off = 0.22;
 L = 4;
-Nc = 1000;
+Nc = 100;
 lambda = 30;
 sig = load('r_sequence_4_8dB.mat');
 
 sig_up_rcv = rrcf(sig.r, taps, T, roll_off, L);
-%epsilon = t_estimator(sig_up_rcv, Nc);    % use if epsilon changes in time
-epsilon = t_estimator(sig_up_rcv);        % use if epsilon is constant
+epsilon = t_estimator(sig_up_rcv, Nc);    % use if epsilon changes in time
+%epsilon = t_estimator(sig_up_rcv);        % use if epsilon is constant
 
 sig_rcv = downsample(sig_up_rcv, L);
 
@@ -26,12 +26,11 @@ img_uint = uint8(bi2de(img_bit8));
 img_m = reshape(img_uint, [], img_size(1))';
 
 clf
-subplot(2,1,1)
+subplot(3,1,1)
 imshow(img_m);
 
 %%%%%%%%%%% interpolate linear %%%%%%%%%%%%%%%%%
-sig_sync = interpolate(sig_rcv, epsilon);
-
+sig_sync = interpolate(sig_rcv, epsilon, 0);
 start_point = frame_sync(sig_sync, lambda);
 img_size = sig.image_size;
 img = sig_sync(start_point:start_point+img_size(1)*img_size(2)*4-1);
@@ -39,10 +38,30 @@ img = sig_sync(start_point:start_point+img_size(1)*img_size(2)*4-1);
 bit = demap(img);
 
 ber2 = sum(xor(bit, sig.ber_pn_seq))/length(bit)
+gain12 = (ber1-ber2)/ber2*100
 
 img_bit8 = reshape(bit, 8, [])';
 img_uint = uint8(bi2de(img_bit8));
 img_m = reshape(img_uint, [], img_size(1))';
 
-subplot(2,1,2)
+subplot(3,1,2)
+imshow(img_m);
+
+%%%%%%%%%%% interpolate cubic %%%%%%%%%%%%%%%%%
+sig_sync = interpolate(sig_rcv, epsilon, 1);
+start_point = frame_sync(sig_sync, lambda);
+img_size = sig.image_size;
+img = sig_sync(start_point:start_point+img_size(1)*img_size(2)*4-1);
+
+bit = demap(img);
+
+ber3 = sum(xor(bit, sig.ber_pn_seq))/length(bit)
+gain23 = (ber2-ber3)/ber3*100
+gain13 = (ber1-ber3)/ber3*100
+
+img_bit8 = reshape(bit, 8, [])';
+img_uint = uint8(bi2de(img_bit8));
+img_m = reshape(img_uint, [], img_size(1))';
+
+subplot(3,1,3)
 imshow(img_m);
